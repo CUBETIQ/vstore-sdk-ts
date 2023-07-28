163aa998694857ba97b704296edcce508d744f24
Sambo Chea <7059827+sombochea@users.noreply.github.com>
2023-07-28 10:58:22 +0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_VSTORE_URL = 'https://vstore.onrender.com';
+export const DEFAULT_VSTORE_URL = 'https://vstore-sg1.fly.dev'; // 'https://vstore.onrender.com';
 export const SDK_ORIGIN_HEADER_PREFIX = 'x-sdk-origin';
 export const PROJECT_ID_HEADER_PREFIX = 'x-project-id';
 export const API_KEY_HEADER_PREFIX = 'x-api-key';
